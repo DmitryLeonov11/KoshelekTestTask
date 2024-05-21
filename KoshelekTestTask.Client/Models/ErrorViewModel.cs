7d6f6c45cfe00e5848e2c8661bc87683811f1f4b
DmitriyLeonov <dleonov701@gmail.com>
2024-05-21 23:29:42 +0300

    client changed

---
+++ KoshelekTestTask.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KoshelekTestTask.Client.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
